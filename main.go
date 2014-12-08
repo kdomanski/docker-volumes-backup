3a8032498c34c4d8b82799b492e4a3271957e286
Kamil Domanski <kdomanski@kdemail.net>
2014-12-08 21:49:19 +0100

    refactored main

--- main.go
+++ main.go
@@ -14,68 +14,77 @@ func main() {
 	config := GetConfig()
 	fmt.Println(config)
 
-	// Init the client
-	client, err := docker.NewClient("unix:///var/run/docker.sock")
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	toPause, err := getRunningContainers(client)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	err = pauseContainers(client, toPause)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	log.Printf("Paused %d cointainers.\n", len(toPause))
-
-	volumes := getDataVolumes(client)
-
 	// set destination for all backups
 	dst := "/tmp/"
 	t := time.Now().UTC()
 	datetime := fmt.Sprintf("%d-%02d-%02d_%02d-%02d-%02d", t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second())
 	dst += datetime + "/"
-	err = os.Mkdir(dst, 0700)
+	err := os.Mkdir(dst, 0700)
 	if err != nil {
 		log.Fatalf("Could not create backup destination: %s", dst)
 	}
+	defer eraseFolder(dst)
 
-	wipetmp := func() {
-		err := os.RemoveAll(dst)
-		if err != nil {
-			log.Fatalf("Failed to remove archives at %s", dst)
-		}
-		log.Printf("Removed archives at %s", dst)
-	}
-	defer wipetmp()
-
-	log.Printf("%d data volumes to backup in %s .", len(volumes), dst)
-
-	for _, vol := range volumes {
-		file, err := backupVolume(client, dst, vol)
-		if err != nil {
-			wipetmp()
-			log.Fatal(err.Error())
-		}
-		log.Printf(" * %s", path.Base(file))
-	}
-
-	err = unpauseContainers(client, toPause)
-	if err != nil {
-		wipetmp()
-		log.Fatal(err.Error())
-	}
-	log.Printf("Unpaused %d cointainers.\n", len(toPause))
+	archiveVolumes(dst)
 
 	err = deliverToFTP(config, dst)
 	if err != nil {
-		wipetmp()
-		log.Fatalf("Failed uploading to FTP: %s", err.Error())
+		log.Panic("Failed uploading to FTP: %s", err.Error())
 	}
 	log.Println("FTP upload successful")
+}
 
+func eraseFolder(destination string) error {
+	err := os.RemoveAll(destination)
+	if err != nil {
+		log.Printf("Failed to remove archives at %s", destination)
+		return err
+	}
+	log.Printf("Removed archives at %s", destination)
+	return nil
+}
+
+func archiveVolumes(destination string) ([]string, error) {
+	// Init the client
+	client, err := docker.NewClient("unix:///var/run/docker.sock")
+	if err != nil {
+		return nil, err
+	}
+
+	toPause, err := getRunningContainers(client)
+	if err != nil {
+		return nil, err
+	}
+
+	err = pauseContainers(client, toPause)
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("Paused %d cointainers.\n", len(toPause))
+
+	defer func() {
+		err = unpauseContainers(client, toPause)
+		if err != nil {
+			log.Panic(err.Error())
+		}
+		log.Printf("Unpaused %d cointainers.\n", len(toPause))
+	}()
+
+	volumes := getDataVolumes(client)
+	log.Printf("%d data volumes to backup in %s .", len(volumes), destination)
+
+	files := make([]string, 0)
+
+	for _, vol := range volumes {
+		file, err := backupVolume(client, destination, vol)
+		if err != nil {
+			return files, err
+		}
+		files = append(files, file)
+		log.Printf(" * %s", path.Base(file))
+	}
+
+	return files, nil
 }
 
 func getRunningContainers(cli *docker.Client) ([]string, error) {
